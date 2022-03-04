b3e65ab5650015cc8bf8ed46b39da542ddfae40f
Sedona Thomas <sntsregister@gmail.com>
2022-03-04 13:59:57 -0500

    Update passwords.py

--- widgets/passwords.py
+++ widgets/passwords.py
@@ -8,6 +8,7 @@ __author__ = "Sedona Thomas"
 __date__ = "03/04/2022"
 __version__ = "1.0.1"
 __maintainer__ = "Sedona Thomas"
+__links__ = ["https://github.com/sedona-thomas/Navagetur"]
 __email__ = "sedona.thomas@columbia.edu"
 
 import random
