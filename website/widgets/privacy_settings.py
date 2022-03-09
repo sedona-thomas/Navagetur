b8c9b9aa15696cd06cbe8a085dfa0d29cb8fe80b
Sedona Thomas <sntsregister@gmail.com>
2022-03-08 23:03:26 -0500

    Setting up settings widget

---
+++ website/widgets/privacy_settings.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Random password generator and password security checker
+"""
+
+__author__ = "Sedona Thomas"
+__date__ = "03/08/2022"
+__version__ = "1.0.1"
+__maintainer__ = "Sedona Thomas"
+__links__ = ["https://github.com/sedona-thomas/Navagetur"]
+__email__ = "sedona.thomas@columbia.edu"
+
+import random
+import string
+import math
+import time
+import os
+
+
+class Password(object):
