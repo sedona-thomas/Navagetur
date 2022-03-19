3bb92900cf0c06809b47f68a0422ee0bc0f6d21a
Sedona Thomas <sntsregister@gmail.com>
2022-03-18 20:03:16 -0400

    Testing python GUI

--- Flask/run.py
+++ Flask/run.py
@@ -1,8 +1,6 @@
-from my_app import app
 import sys
+from my_app import app
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        app.run(host="0.0.0.0", port=int(sys.argv[1]), debug=True)
-    else:
-        app.run(host="0.0.0.0", port=5000, debug=True)
+    port = 5000 if len(sys.argv) > 1 else sys.argv[1]
+    app.run(host="0.0.0.0", port=port, debug=True)
