cdb15d01dafdb57f6dc5e582071b41663d079131
Sedona Thomas <sntsregister@gmail.com>
2022-03-18 20:37:58 -0400

    Update run.py

--- Flask/run.py
+++ Flask/run.py
@@ -2,5 +2,5 @@ import sys
 from my_app import app
 
 if __name__ == '__main__':
-    port = 5000 if len(sys.argv) > 1 else sys.argv[1]
+    port = 5000 if len(sys.argv) == 1 else sys.argv[1]
     app.run(host="0.0.0.0", port=port, debug=True)
