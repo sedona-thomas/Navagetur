0fa1ba4746fb78d17eaf7f1f9ccf4ee53410fc49
Sedona Thomas <sntsregister@gmail.com>
2022-03-19 11:30:11 -0400

    pages opening properly

--- Flask App/navagetur/main.py
+++ Flask App/navagetur/main.py
@@ -18,7 +18,41 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/education.html")
+def education():
+    return render_template("education.html")
+
+
 @app.route("/widgets.html")
 def widgets():
     return render_template("widgets.html")
 
+
+@app.route("/password.html")
+def password():
+    return render_template("password.html")
+
+
+@app.route("/account_security.html")
+def account_security():
+    return render_template("account_security.html")
+
+
+@app.route("/law_locator.html")
+def law_locator():
+    return render_template("law_locator.html")
+
+
+@app.route("/personal_uniqueness.html")
+def personal_uniqueness():
+    return render_template("personal_uniqueness.html")
+
+
+@app.route("/table.html")
+def table():
+    return render_template("table.html")
+
+
+@app.route("/citations.html")
+def citations():
+    return render_template("citations.html")
