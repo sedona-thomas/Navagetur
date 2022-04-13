02633d4e2a221c315f4de197faeb3dda91ca520f
Sedona Thomas <sedona.thomas@columbia.edu>
2022-04-12 22:27:04 -0400

    Create EncryptedJSONDatabase.py

---
+++ Flask App/navagetur/widgets/EncryptedJSONDatabase.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Sedona Thomas"
+__date__ = "03/08/2022"
+__version__ = "1.0.1"
+__maintainer__ = "Sedona Thomas"
+__links__ = ["https://github.com/sedona-thomas/Navagetur"]
+__email__ = "sedona.thomas@columbia.edu"
+
+
+import json
+import os
+from navagetur.widgets.encryption import *
+
+
+class EncryptedJSONDatabase(object):
+    def __init__(self, filename, password):
+        self._filename = filename
+        self._password = password
+        self._database = self.read()
+
+    def __iter__(self):
+        ''' Returns the Iterator object '''
+        return JSONDatabaseIterator(self)
+
+    def add(self, entry):
+        self._database.append(entry)
+        self.write()
+
+    def getAll(self):
+        return self._database
+
+    def read(self):
+        if os.stat(self._filename).st_size == 0:
+            return []
+        else:
+            crypter = DataEncryption(self._password)
+            with open(self._filename, "rb") as file:
+                plaintext = crypter.decrypt(file.read())
+                if self._is_json(plaintext):
+                    return json.load(plaintext)
+                else:
+                    return []
+
+    def write(self):
+        with open(self._filename, "wb") as f:
+            if self._encryption:
+                crypter = DataEncryption(self._password)
+                f.write(crypter.encrypt(json.dumps(self._database)))
+            else:
+                f.write(json.dumps(self._database))
+
+    def getTable(self):
+        return "<table>" + self.getTableBody() + "</table>"
+
+    def getTableBody(self):
+        tb = ""
+        for row in self._database:
+            tb += "<tr>"
+            for k in row:
+                cell = str(row[k]) if type(
+                    row[k]) is not list else ", ".join(row[k])
+                tb += "<td>" + cell + "</td>"
+            tb += "</tr>"
+        return tb
+
+    def _is_json(self, plaintext):
+        try:
+            json.load(plaintext)
+            return True
+        except:
+            return False
+
+
+class JSONDatabaseIterator:
+    ''' Iterator class '''
+
+    def __init__(self, database):
+        self._database = database
+        self._index = 0
+
+    def __next__(self):
+        ''''Returns the next value from database object's list'''
+        if self._index < (len(self._database._database)):
+            result = self._database._database[self._index]
+            self._index += 1
+            return result
+        raise StopIteration
